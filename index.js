70c84c88761c5dbf7d0bcc1ba4cbce35a4261386
SajalAhmeed <sazal.me@gmail.com>
2019-01-06 19:28:52 +0600

    first-commit

--- index.js
+++ index.js
@@ -1,76 +1,129 @@
-const express = require('express');
-const Joi = require('joi');
-const morgan = require('morgan');
-const config = require('config');
-const debug = require('debug')('app:startup');
-const courses = require('./routes/courses')
-const home = require('./routes/home')
+const mongoose = require('mongoose');
+//>>> md c:\\data (tutorial: 3)
+const config = {
+   // autoIndex: false,
+    useNewUrlParser: true
+  };
+
+mongoose.connect("mongodb://localhost:27017/playground", config)
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(() => console.log('Could not connect to MongDB..', err))
 
 
-const logger = require('./logger')
-
-
-const app = express();
-
-app.set('view engine', 'pug');
-// app.set('views' './custome_views') # it's means if you want to store your template in custom folder.
-
-
-
-//process.env.NODE_ENV // if set NODE_ENV not define then it show undefine
-
-
-/*built in middlwere in express*/
-app.use(express.json()); // it's a built in middleware. Which is  parses the boby of the middleware and if there is a json object it will populate on req.body
-  
-app.use(express.urlencoded({extended: true})); // this  middleware function parse incoming request with url example:https://1234.com/key=value&n=24
-
-app.use(express.static('public'));
-
-/* end express middlewere*/
-
-app.use('/api/courses', courses);
-
-app.use('/', home);
-
-
-console.log('Appliction Name: ' + config.get('name'));
-console.log('Mail  Server: ' + config.get('mail.host'));
-console.log('Mail  Password: ' + config.get('mail.password'));
-
-
-
-if(app.get('env') === 'development'){ // it's means morgan only work in development machine .default app.get('env') return development.
-	app.use(morgan('tiny')) // it's show log message in tiny format when do api request.
-	//console.log("Morgan is enable")
-
-	debug("Morgan is enable");
-}
-
-app.use(logger); //customer middleware function
-
-
-/*
-app instance have some method():
-
-app.get(); // it's take two argument 1.url , 2. call back function (req, res) req = request and res = respose
-app.post();
-app.put();
-app.delete();
-
-
-call back function also called route handler :)
-*/
-
-// Environment Variables 
-// Change something (check branch work :).
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`)
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
 })
 
+// Classes, objects
+// Course, nodeCourse
+
+const Course = mongoose.model('Course', courseSchema);
+
+
+async function createCourse() {
+    const course = new Course({
+        name: 'React Course',
+        author: 'Sajal',
+        tags: ['react', 'front-end'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+
+    console.log(result);
+}
+
+//createCourse();
+
+async function getCourses() { 
+
+    // comparison operator
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater thant)
+    // gte (greater than or equal to )
+    // lt (less than)
+    // lte (less than or equal to)
+    // in 
+    // nin (not in)
+
+    //Logical Operator
+    // or
+    //and
+    // /api/couses?pageNumber=2&pageSize=10
+
+    const courses = await Course
+        .find({isPublished: true})
+        .select({name: 1})
+        .sort({name: 1})
+        //.find({author: 'Mosh', isPublished: true})
+        //.find({price: {$gt: 10, $lte: 20}})
+        //.find({ price: {$in: [10, 15, 20]}})
+        //.find()
+        //.or([{ author: 'Mosh'}, { isPublished: true}])
+        //.find({ author: /^Mosh/ })
+        //.limit(10)
+        //.sort({name: -1 }) //1 assending order -1 decending order
+        //.select({ name: 1, tags: 1})
+        //.count() // Deprecated
+        //.countDocuments()
+
+    console.log(courses);
+}
+
+
+getCourses();
+
+
+async function updateCourse(id){
+    // Approach: Query first 
+    // findById()
+    // Modify its properties
+    // Save();
+    //const course = await Course.findById(id);
+
+
+    
+   // if(!course) return;
+    // approach 01
+    /*
+    course.isPublished = false;
+    course.author = 'Another Author';
+    */
+    //approahc 02
+    /*
+    course.set({
+        isPublished: true,
+        author: 'Another Author'
+    }); */
+   //const result = await course.save();
+
+   //console.log(result);
+
+    // Approach Update first
+    // Update directly 
+    // Optionally: get the updated document
+
+    const result = await Course.updateOne({_id: id}, {
+        $set: {
+            name:"Ami toke Chini na.",
+            author: 'Abul',
+            isPublished: false,
+        }
+    });
+    console.log(result);
+}   
+//updateCourse('5c31f8c15eb32120e055ec56');
 
 
 
+async function removeCourse(id){
+
+    const result = await Course.deleteOne({ _id: id})
+    console.log(result);
+}   
+removeCourse('5c31f8c15eb32120e055ec56')
